04bb0dec7170d8a7d1c70448af3eda237ceda3f9
bobpepers <bob.pepers@gmail.com>
2023-11-17 03:24:02 +0100

    refactor: ♻️ class -> fc (ImportWallet)

--- src/popup/pages/ImportWallet/index.tsx
+++ src/popup/pages/ImportWallet/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, FC } from 'react';
+import React, { useEffect } from 'react';
 import {
   Typography,
   TextField,
@@ -11,111 +11,102 @@ import {
   Select,
   MenuItem,
 } from '@mui/material';
-import { WithStyles } from '@mui/styles';
-import withStyles from '@mui/styles/withStyles';
 import { inject, observer } from 'mobx-react';
-
-import styles from './styles';
 import NavBar from '../../components/NavBar';
 import BorderTextField from '../../components/BorderTextField';
 import AppStore from '../../stores/AppStore';
 import { IMPORT_TYPE } from '../../../constants';
+import useStyles from './styles';
 
 interface IProps {
   classes: Record<string, string>;
   store: AppStore;
 }
 
-interface IState {
-  importStore: any;
-  walletStore: any;
-}
+const ImportWallet: React.FC<IProps> = ({ store }) => {
+  const { importStore } = store;
+  const classes = useStyles();
 
-@inject('store')
-@observer
-class ImportWallet extends Component<WithStyles<typeof styles> & IProps, IState> {
-  public componentWillUnmount() {
-    this.props.store.importStore.reset();
-  }
+  useEffect(() => {
+    return () => {
+      importStore.reset();
+    };
+  }, [importStore]);
 
-  public render() {
-    const { classes, store: { importStore } }: any = this.props;
-
-    return (
-      <div className={classes.root}>
-        <NavBar hasNetworkSelector title="" />
-        <div className={classes.contentContainer}>
-          <Typography className={classes.headerText}>Import Wallet</Typography>
-          <div className={classes.inputContainer}>
-            <div className={classes.fieldContainer}>
-              <TypeField {...this.props} />
-              <TextField
-                className={classes.mnemonicPrKeyTextField}
-                autoFocus
-                required
-                multiline
-                rows={5}
-                type="text"
-                placeholder={`Enter your ${importStore.importType} here to import your wallet.`}
-                onChange={(e) => importStore.mnemonicPrivateKey = e.target.value}
-                InputProps={{
-                  disableUnderline: true,
-                  classes: { input: classes.mnemonicPrKeyFieldInput },
-                }}
-              />
-              {!!importStore.mnemonicPrivateKey && importStore.privateKeyError && (
-                <Typography className={classes.errorText}>{importStore.privateKeyError}</Typography>
-              )}
-                <BorderTextField
-                  classNames={classes.borderTextFieldContainer}
-                  placeholder="Wallet name"
-                  error={importStore.walletNameTaken}
-                  errorText={importStore.walletNameError}
-                  onChange={(e: any) => importStore.accountName = e.target.value}
-                  onEnterPress={importStore.importMnemonicOrPrKey}
-                />
-            </div>
-          </div>
-          <div>
-            <Button
-              className={classes.importButton}
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={importStore.importMnemonicOrPrKey}
-              disabled={importStore.mnemonicPrKeyPageError}
-            >
-              Import
-            </Button>
-            <Button
-              className={classes.cancelButton}
-              fullWidth
-              color="primary"
-              onClick={importStore.cancelImport}
-            >
-              Cancel
-            </Button>
+  return (
+    <div className={classes.root}>
+      <NavBar hasNetworkSelector title="" />
+      <div className={classes.contentContainer}>
+        <Typography className={classes.headerText}>Import Wallet</Typography>
+        <div className={classes.inputContainer}>
+          <div className={classes.fieldContainer}>
+            <TypeField classes={classes} store={store} />
+            <TextField
+              className={classes.mnemonicPrKeyTextField}
+              autoFocus
+              required
+              multiline
+              rows={5}
+              type="text"
+              placeholder={`Enter your ${importStore.importType} here to import your wallet.`}
+              onChange={(e) => (importStore.mnemonicPrivateKey = e.target.value)}
+              InputProps={{
+                disableUnderline: true,
+                classes: { input: classes.mnemonicPrKeyFieldInput },
+              }}
+            />
+            {!!importStore.mnemonicPrivateKey && importStore.privateKeyError && (
+              <Typography className={classes.errorText}>{importStore.privateKeyError}</Typography>
+            )}
+            <BorderTextField
+              classNames={classes.borderTextFieldContainer}
+              placeholder="Wallet name"
+              error={importStore.walletNameTaken}
+              errorText={importStore.walletNameError}
+              onChange={(e: any) => (importStore.accountName = e.target.value)}
+              onEnterPress={importStore.importMnemonicOrPrKey}
+            />
           </div>
         </div>
-        <ErrorDialog {...this.props} />
+        <div>
+          <Button
+            className={classes.importButton}
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={importStore.importMnemonicOrPrKey}
+            disabled={importStore.mnemonicPrKeyPageError}
+          >
+            Import
+          </Button>
+          <Button
+            className={classes.cancelButton}
+            fullWidth
+            color="primary"
+            onClick={importStore.cancelImport}
+          >
+            Cancel
+          </Button>
+        </div>
       </div>
-    );
-  }
-}
+      <ErrorDialog store={store} />
+    </div>
+  );
+};
 
-const Heading = withStyles(styles, { withTheme: true })(({ classes, name }: any) => (
+const Heading: React.FC<any> = ({ classes, name }) => (
   <Typography className={classes.fieldHeading}>{name}</Typography>
-));
+);
 
-const TypeField = observer(({ classes, store: { importStore } }: any) => (
+const TypeField: React.FC<any> = observer(({ classes, store }) => (
   <div className={classes.fieldContainer}>
-    <Heading name="Select Type" />
+    <Heading name="Select Type" classes={classes} />
     <div className={classes.fieldContentContainer}>
       <Select
         className={classes.typeSelect}
         disableUnderline
-        value={importStore.importType}
-        onChange={(event) => importStore.changeImportType(event.target.value)}
+        value={store.importStore.importType}
+        onChange={(event) => store.importStore.changeImportType(event.target.value)}
       >
         <MenuItem value={IMPORT_TYPE.MNEMONIC}>
           <Typography className={classes.menuItemTypography}>Seed Phrase</Typography>
@@ -128,18 +119,21 @@ const TypeField = observer(({ classes, store: { importStore } }: any) => (
   </div>
 ));
 
-const ErrorDialog: FC<any> = observer(({ store: { importStore }}: any) => (
+const ErrorDialog: React.FC<any> = observer(({ store }) => (
   <Dialog
-    open={importStore.importMnemonicPrKeyFailed}
-    onClose={() => importStore.importMnemonicPrKeyFailed = false}>
-    <DialogTitle>{`Invalid ${importStore.importType}`}</DialogTitle>
+    open={store.importStore.importMnemonicPrKeyFailed}
+    onClose={() => (store.importStore.importMnemonicPrKeyFailed = false)}
+  >
+    <DialogTitle>{`Invalid ${store.importStore.importType}`}</DialogTitle>
     <DialogContent>
       <DialogContentText>This wallet has already been imported.</DialogContentText>
     </DialogContent>
     <DialogActions>
-      <Button onClick={() => importStore.importMnemonicPrKeyFailed = false} color="primary">Close</Button>
+      <Button onClick={() => (store.importStore.importMnemonicPrKeyFailed = false)} color="primary">
+        Close
+      </Button>
     </DialogActions>
   </Dialog>
 ));
 
-export default withStyles(styles)(ImportWallet);
+export default inject('store')(observer(ImportWallet));
