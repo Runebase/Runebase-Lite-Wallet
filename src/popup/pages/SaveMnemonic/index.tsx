ace5f3729b32241e8cc9c50439f1f7eef7f0ed5f
bobpepers <bob.pepers@gmail.com>
2023-11-17 14:22:36 +0100

    refactor: ♻️ class -> fc (SaveMnemonic)

--- src/popup/pages/SaveMnemonic/index.tsx
+++ src/popup/pages/SaveMnemonic/index.tsx
@@ -1,67 +1,62 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Button } from '@mui/material';
-import { WithStyles } from '@mui/styles';
-import withStyles from '@mui/styles/withStyles';
 import { inject, observer } from 'mobx-react';
 import cx from 'classnames';
-
 import NavBar from '../../components/NavBar';
 import AppStore from '../../stores/AppStore';
-import styles from './styles';
+import useStyles from './styles';
 const strings = require('../../localization/locales/en_US.json');
-interface IProps extends WithStyles<typeof styles> {
+
+interface IProps {
   store: AppStore;
 }
 
-@inject('store')
-@observer
-class SaveMnemonic extends Component<IProps, NonNullable<unknown>> {
-  public componentDidMount() {
-     console.log('SaveMnemonic');
-     // console.log(this.props.store);
-     this.props.store.saveMnemonicStore.generateMnemonic();
-  }
+const SaveMnemonic: React.FC<IProps> = ({ store }) => {
+  const classes = useStyles();
+  useEffect(() => {
+    console.log('SaveMnemonic');
+    // console.log(store);
+    store.saveMnemonicStore.generateMnemonic();
 
-  public render() {
-    const { classes, store } = this.props;
+    return () => {
+      // Cleanup code (if needed) when the component unmounts
+    };
+  }, [store.saveMnemonicStore]);
 
-    return (
-      <div className={classes.root}>
-        <NavBar hasBackButton title={''} />
-        <div className={classes.contentContainer}>
-          <div className={classes.topContainer}>
-            <Typography className={classes.walletCreatedHeader}>
-              {strings['saveMnemonic.walletCreated']}
-            </Typography>
-            <Typography className={classes.mnemonicText}>
-              {store.saveMnemonicStore.mnemonic}
-            </Typography>
-            <Typography className={classes.warningText}>
-              {strings['saveMnemonic.warningText']}
-            </Typography>
-          </div>
-          <Button
-            className={cx(classes.actionButton, 'marginBottom')}
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={() => store.saveMnemonicStore.createWallet(false)}
-          >
-            I Copied It Somewhere Safe
-          </Button>
-          <Button
-            className={classes.actionButton}
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={() => store.saveMnemonicStore.createWallet(true)}
-          >
-            Save To File
-          </Button>
+  return (
+    <div className={classes.root}>
+      <NavBar hasBackButton title={''} />
+      <div className={classes.contentContainer}>
+        <div className={classes.topContainer}>
+          <Typography className={classes.walletCreatedHeader}>
+            {strings['saveMnemonic.walletCreated']}
+          </Typography>
+          <Typography className={classes.mnemonicText}>{store.saveMnemonicStore.mnemonic}</Typography>
+          <Typography className={classes.warningText}>
+            {strings['saveMnemonic.warningText']}
+          </Typography>
         </div>
+        <Button
+          className={cx(classes.actionButton, 'marginBottom')}
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={() => store.saveMnemonicStore.createWallet(false)}
+        >
+          I Copied It Somewhere Safe
+        </Button>
+        <Button
+          className={classes.actionButton}
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={() => store.saveMnemonicStore.createWallet(true)}
+        >
+          Save To File
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withStyles(styles)(SaveMnemonic);
+export default inject('store')(observer(SaveMnemonic));
