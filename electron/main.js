4539ce2e4b8a19c5eb7ff14a49ec972208ee5d6b
bobpepers <bob.pepers@gmail.com>
2023-12-04 21:37:49 +0100

    perf: ⚡️ setMinimumSize (electron browserwindow)

--- electron/main.js
+++ electron/main.js
@@ -24,6 +24,7 @@ function createWindow() {
 
   // Remove the default menu bar
   Menu.setApplicationMenu(null);
+  mainWindow.setMinimumSize(350, 600);
 
   mainWindow.on('closed', function () {
     mainWindow = null;
