498960583416cbf24c2e1cb70e42e2a9efd08d74
bobpepers <bob.pepers@gmail.com>
2023-12-09 15:49:53 +0100

    build: 🔨 improved build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,60 @@
 #!/bin/bash
 
+set -e
+
+# Check if required commands are available
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+for cmd in yarn jq zip cordova electron-builder webpack; do
+    if ! command_exists "$cmd"; then
+        echo "Error: $cmd not found. Please install it before running this script."
+        exit 1
+    fi
+done
+
+# Define version for chrome extension zip
+version=$(jq -r .version package.json)
+
+# Clean and prepare directories
 yarn clean
-mkdir dist
+mkdir -p dist
 rm -rf dist-electron/*
 rm -rf cordova/www/*
+rm -rf compiled_files/*
+
+# Run the script to create an empty thunk (assuming it doesn't need special permissions)
 ./scripts/create-empty-thunk.sh
+
+# Build the application with Webpack
 webpack --progress --config webpack.prod.config.js
+
+# Copy the build output to the Cordova www directory
 cp -R dist/* cordova/www
+
+# Change to the Cordova directory and build the Android app
 cd cordova
 cordova build android
 cordova build android --release
 cd ..
+
+# Build the Electron app for Windows and Linux
 electron-builder --win --linux -c electron-builder-config.js
+
+# Create the zip file for the Chrome extension
+zip -r "Runebase-Lite-Wallet-$version.zip" dist/*
+
+# Create a directory for compiled files
+compiled_dir="compiled_files"
+mkdir -p "$compiled_dir"
+
+# Copy relevant compiled files to the new directory with renamed files
+cp -r cordova/platforms/android/app/build/outputs/apk/debug/app-debug.apk "$compiled_dir/Runebase-Lite-Wallet-v$version-debug.apk"
+cp -r cordova/platforms/android/app/build/outputs/bundle/release/app-release.aab "$compiled_dir/Runebase-Lite-Wallet-v$version-release.aab"
+cp -r dist-electron/*.AppImage "$compiled_dir"
+cp -r dist-electron/*.exe "$compiled_dir"
+cp "Runebase-Lite-Wallet-$version.zip" "$compiled_dir"
+
+# Display a message indicating where the compiled files are located
+echo "Compiled files are in the '$compiled_dir' directory."
