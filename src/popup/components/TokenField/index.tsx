f61ad674c4e21b45c2d43504ae2cf308891533a6
bobpepers <bob.pepers@gmail.com>
2023-12-19 13:02:43 +0100

    style: 🎨 TokenField (Select): Add token images

--- src/popup/components/TokenField/index.tsx
+++ src/popup/components/TokenField/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Select, MenuItem, FormControl, InputLabel, Typography } from '@mui/material';
+import { Select, MenuItem, FormControl, InputLabel, Typography, Box } from '@mui/material';
 import { observer } from 'mobx-react';
 import RRCToken from '../../../models/RRCToken';
+import { TOKEN_IMAGES } from '../../../constants'; // Assuming you have a constant file for token images
 
 const TokenField = observer(({ sendStore }: any) => (
-  <FormControl
-    fullWidth
-    sx={{marginBottom: '8px'}}
-  >
+  <FormControl fullWidth sx={{ marginBottom: '8px' }}>
     <InputLabel id="token-label">Token</InputLabel>
     <Select
       labelId="token-label"
@@ -16,8 +14,17 @@ const TokenField = observer(({ sendStore }: any) => (
       onChange={(event) => sendStore.changeToken(event.target.value)}
     >
       {sendStore.tokens.map((token: RRCToken) => (
-        <MenuItem key={token.symbol} value={token.symbol}>
-          <Typography>{token.symbol}</Typography>
+        <MenuItem key={token.symbol} value={token.symbol} sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', height: '24px' }}>
+            {TOKEN_IMAGES[token.address] && (
+              <img
+                src={TOKEN_IMAGES[token.address]}
+                alt={token.symbol}
+                style={{ marginRight: '8px', height: '24px', width: '24px', objectFit: 'contain' }}
+              />
+            )}
+            <Typography>{token.symbol}</Typography>
+          </Box>
         </MenuItem>
       ))}
     </Select>
